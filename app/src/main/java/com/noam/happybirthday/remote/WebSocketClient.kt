a64adb9a3c157a59566443f4bf452cca6a8e1399
Noam Weingarten <noam.weingarten@sbdinc.com>
2025-04-07 17:12:03 +0300

    changed the retry implementation to better manage the coroutine creations.

--- app/src/main/java/com/noam/happybirthday/remote/WebSocketClient.kt
+++ app/src/main/java/com/noam/happybirthday/remote/WebSocketClient.kt
@@ -9,11 +9,11 @@ import io.ktor.client.plugins.websocket.WebSockets
 import io.ktor.client.plugins.websocket.wss
 import io.ktor.websocket.Frame
 import io.ktor.websocket.readText
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
 class WebSocketClient {
@@ -22,7 +22,6 @@ class WebSocketClient {
     private lateinit var job : Job
     private var received = ""
     private lateinit var listener : WebSocketListener
-    private var retryCounter = 0
     private val client = HttpClient {
         install(HttpTimeout) {
             requestTimeoutMillis = 5000
@@ -38,64 +37,59 @@ class WebSocketClient {
         this.url = "ws://$ipAddress/nanit"
     }
 
+
+    private val handler = CoroutineExceptionHandler { _, exception ->
+        Log.e("Exception","CoroutineExceptionHandler got $exception")
+        listener.onError(Exception("Max retry attempts reached"))
+        job.cancel()
+    }
+
     fun connect(eventsListener: WebSocketListener) {
         Log.d("WebSocketClient", "connect: $url")
         this.listener = eventsListener
         listener.onConnecting()
-        job = CoroutineScope(Dispatchers.Default).launch {
-            while (isActive) {
-                try {
-                    client.wss(url) {
-                        listener.onConnected()
-                        retryCounter = 0
-
-                        Log.d("Websocket", "connect: sending the happyBirthday frame")
-                        outgoing.trySend(Frame.Text(SERVER_MESSAGE_STARTER))
-                        try {
-                            for (frame in incoming) {
-                                if (frame is Frame.Text) {
-                                    received = frame.readText()
-                                    listener.onMessage(received)
-                                    Log.d("Websocket", "incoming received message $received")
-                                }
-                                if (frame is Frame.Close) {
-                                    Log.d("Websocket", "incoming received close frame")
-                                    listener.onDisconnected()
-                                    break
-                                }
-                            }
-                        } catch (e: Exception) {
-                            Log.e("WebSocketClient", "Error in WebSocket connection: ${e.message}")
-                            listener.onDisconnected()
-                        }
-                    }
-                } catch (e: Exception) {
-                    Log.e("WebSocketClient", "Error connecting to WebSocket: ${e.message}")
-                    listener.onDisconnected()
-                    retry()
-                    return@launch
-                }
-                Log.d("WebSocketClient", "WebSocket connection closed, reconnecting...")
+        job = CoroutineScope(Dispatchers.Default).launch(handler) {
+            retry(numberOfRetries = 3, delayBetweenRetries = 2000L) {
+                connectToWebSocket()
             }
-            Log.d("WebSocketClient", "WebSocket connection closed cancelling job")
-            listener.onDisconnected()
         }
     }
 
-    private fun retry() {
-        job.cancel()
-        CoroutineScope(Dispatchers.Default).launch {
-            if (retryCounter < 3) {
-                retryCounter++
-                listener.onConnecting()
-                delay(2000)
-                connect(listener)
-            } else {
-                Log.d("WebSocketClient", "Max retry attempts reached. Not reconnecting.")
-                listener.onError(Exception("Max retry attempts reached"))
-                retryCounter = 0
+    private suspend fun connectToWebSocket() {
+        client.wss(url) {
+            listener.onConnected()
+
+            Log.d("Websocket", "connect: sending the happyBirthday frame")
+            outgoing.trySend(Frame.Text(SERVER_MESSAGE_STARTER))
+            for (frame in incoming) {
+                if (frame is Frame.Text) {
+                    received = frame.readText()
+                    listener.onMessage(received)
+                    Log.d("Websocket", "incoming received message $received")
+                }
+                if (frame is Frame.Close) {
+                    Log.d("Websocket", "incoming received close frame")
+                    listener.onDisconnected()
+                    break
+                }
             }
         }
+    }
+
+    private suspend fun <T> retry(
+        numberOfRetries: Int,
+        delayBetweenRetries: Long = 100,
+        block: suspend () -> T
+    ): T {
+        repeat(numberOfRetries) {
+            try {
+                return block()
+            } catch (exception: Exception) {
+                Log.e("retry", "Retry got an exception =$exception")
+            }
+            delay(delayBetweenRetries)
+        }
+        return block() // last attempt
     }
 
     fun disconnect() {
